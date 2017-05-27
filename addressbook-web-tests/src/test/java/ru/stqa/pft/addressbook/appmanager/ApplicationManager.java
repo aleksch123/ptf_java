edfffbd18376bd61f387f29bf128984cd4f64f21
aleksch123 <utest.aleksey@gmail.com>
2017-05-27 15:48:46 +1000

    Добавлено использование различных браузеров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,11 +15,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager  {
 
-    FirefoxDriver wd;
+    WebDriver wd;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
     private NavigationHelper navigationHalper;
     private  GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
@@ -27,7 +36,14 @@ public class ApplicationManager  {
     }
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+
+        if (browser== org.openqa.selenium.remote.BrowserType.FIREFOX){
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        } else if (browser== org.openqa.selenium.remote.BrowserType.CHROME){
+            wd = new ChromeDriver();
+        } else if (browser== org.openqa.selenium.remote.BrowserType.IE) {
+            wd = new EdgeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
