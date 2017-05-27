deed5fefc5c519d06b6da554afd1b3fad79870bf
aleksch123 <utest.aleksey@gmail.com>
2017-05-27 17:56:38 +1000

    gggg

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -37,11 +37,11 @@ public class ApplicationManager  {
 
     public void init() {
 
-        if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)){
+        if (browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser.equals( org.openqa.selenium.remote.BrowserType.CHROME)){
+        } else if (browser.equals( BrowserType.CHROME)){
             wd = new ChromeDriver();
-        } else if (browser.equals( org.openqa.selenium.remote.BrowserType.IE)) {
+        } else if (browser.equals( BrowserType.IE)) {
             wd = new EdgeDriver();
         }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
