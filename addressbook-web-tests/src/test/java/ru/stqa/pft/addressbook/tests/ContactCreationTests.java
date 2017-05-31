b18877aa1792bcfecd13e892f7af89d57c5d09e6
aleksch123 <utest.aleksey@gmail.com>
2017-05-31 22:12:11 +1000

    Добавлено сравнение списков после добавление контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -17,13 +18,19 @@ public class ContactCreationTests extends TestBase {
         app.getNavigationHalper().goToMainPage();
         List<ContactData> before =app.getContactHelper().getContactList();
         app.getContactHelper().initContactCreations();
-        app.getContactHelper().fillUserData(new ContactData("John", "Smith", "St", "Jos", "Mr.",
+        ContactData contact = new ContactData("John", "Smith", "St", "Jos", "Mr.",
                 "Global", "10005 NY 5st ave 123",
-                "+1234567890", "johmsmith@yahoo.cpm","Test1"),true);
+                "+1234567890", "johmsmith@yahoo.cpm","Test1");
+        app.getContactHelper().fillUserData(contact,true);
         app.getContactHelper().submitUserCreations();
         app.getNavigationHalper().goToMainPage();
         List<ContactData> after =app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(),before.size()+1);
+
+        before.add(contact);
+
+        contact.setId(after.stream().max( (o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+        Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
     }
 
 
