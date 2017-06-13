0f02f46186b78343580ea7aa348bf9a6bf7d7d54
aleksch123 <utest.aleksey@gmail.com>
2017-06-13 17:00:57 +1000

    пппппп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -11,7 +11,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
 
-    public static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.FIREFOX));
 
 
 
