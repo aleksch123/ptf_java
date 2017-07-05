9ad9667e8be5ab51215b6252977ec6be2585e659
aleksch123 <utest.aleksey@gmail.com>
2017-07-05 16:34:08 +1000

    Реализован FTP helper

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -17,9 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHalper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser)  {
         this.browser = browser;
@@ -31,20 +33,13 @@ public class ApplicationManager {
         String target= System.getProperty("target","local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-            }
-            wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            wd.get(properties.getProperty("web.baseUrl"));
+
 
 
         }
     public void stop() {
-       wd.quit();
+        if (wd!=null){
+       wd.quit();}
     }
  public HttpSession  NewSession() {
      return new HttpSession(this);
@@ -52,4 +47,34 @@ public class ApplicationManager {
  public String getProperty(String key){
         return properties.getProperty(key);
  }
+
+    public RegistrationHelper registration() {
+     if(registrationHalper==null) {
+         registrationHalper = new RegistrationHelper(this);
+     }
+     return registrationHalper;
+    }
+
+    public FtpHelper ftp(){
+     if (ftp==null){
+         ftp= new FtpHelper(this);
+     }
+     return ftp;
+    }
+
+
+    public WebDriver getDriver() {
+     if (wd==null){
+         if (browser.equals(BrowserType.FIREFOX)) {
+             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+         } else if (browser.equals(BrowserType.CHROME)) {
+             wd = new ChromeDriver();
+         } else if (browser.equals(BrowserType.IE)) {
+             wd = new InternetExplorerDriver();
+         }
+         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+         wd.get(properties.getProperty("web.baseUrl"));
+     }
+        return wd;
+    }
 }
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_crypto_master_salt     = 'TXWP0TjEfvLzf8te3fCEvuASS3gaXSuzbnnul/Yyeq8=';
+$g_signup_use_captcha     = OFF;
+
